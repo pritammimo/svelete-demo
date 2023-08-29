8877faa6f5eec27cd411935211ad8cbcec565a70
pritammimo <crpritamsaha@gmail.com>
2023-08-29 22:10:23 +0530

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,9 @@
 <script>
+  import Card from "../components/Card.svelte";
 import quizes from "../data.json";
 import * as images from "../image";
 </script>
 {#each quizes as quiz (quiz.id)}
-<button
-class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2"
->
- <img class="h-44" src={images[quiz.imageUrl]} alt="" /> 
-<div class="flex items-center justify-between py-3 px-2">
-  <h1 class="font-bold">{quiz.name}</h1>
-  <p>{quiz.questions.length} Quiz</p>
-</div>
-</button>
+<Card {quiz}/>
 {/each}
 
